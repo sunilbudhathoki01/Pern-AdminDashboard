3ae92bb0307a61f3dcc89ddbb35ea8e8491d5446
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2026-01-24 19:41:33 +0545

    add database config and schema

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+if (!process.env.DATABASE_URL) throw new Error("Database url is required");
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
